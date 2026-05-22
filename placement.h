2e4a17d7fcf3abd7d67f2bd54553626e90808f5e
trungnotchung <0xtrungnotchung@gmail.com>
2026-05-22 12:45:18 +0700

    mark max sequences as inf

--- placement.h
+++ placement.h
@@ -5,7 +5,7 @@
 #include "fstream"
 #include "timeutil.h"
 
-const int MAX_SEQUENCE = 100000;
+const int MAX_SEQUENCE = 1e9;
 
 /** Place new VCF samples onto an existing reference tree. */
 void placeNewSamplesOntoExistingTree(Params &params);
